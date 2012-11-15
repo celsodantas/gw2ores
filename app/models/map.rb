6c6011b39320c809c6afc7c67d156cded31d0f43
Celso Dantas <celsodantas@gmail.com>
2012-11-15 11:07:28 -0300

    removing the ResetDate usage.

--- app/models/map.rb
+++ app/models/map.rb
@@ -1,9 +1,10 @@
 class Map < ActiveRecord::Base
   attr_accessible :name, :image, :image_mini
-  #has_many :ore_nodes
+  has_many :ore_nodes
 
-  def ore_nodes
-  	reset = ResetDate.last
-  	reset.ore_nodes.where("map_id = #{self.id}")
-  end
+  # ResetDate still needs work!
+  # def ore_nodes
+  # 	reset = ResetDate.last
+  # 	reset.ore_nodes.where("map_id = #{self.id}")
+  # end
 end
