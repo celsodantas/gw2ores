a509248b26c1fcc806c02f9b61ca0c0d90a52de6
Celso Dantas <celsodantas@gmail.com>
2013-02-23 22:17:45 -0600

    removing manual reset feature

--- app/controllers/ore_nodes_controller.rb
+++ app/controllers/ore_nodes_controller.rb
@@ -13,19 +13,6 @@ class OreNodesController < ApplicationController
   	@ore.delete
   end
 
-  def reset
-    @server = Server.find(params[:server])
-    @server.maps.each do |map|
-      map.ore_nodes.destroy_all
-    end
-    
-    new_reset_date = ResetDate.new
-    @server.reset_dates << new_reset_date
-
-    flash[:notice] = "Maps for server #{@server.name} reseted! Now get to work to populate the maps!"
-    redirect_to server_path(:server => @server.id)
-  end
-
   def reset_all
     if params[:key] == ::OreLocations::Application::RESET_KEY
       OreNode.destroy_all
