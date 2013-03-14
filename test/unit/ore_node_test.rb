ca92c38749464ee72effb7bbc552a8062dddc469
Celso Dantas <celsodantas@gmail.com>
2013-03-13 19:13:46 -0500

    removing standart rails test suite

--- test/unit/ore_node_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class OreNodeTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/reset_date_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ResetDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
