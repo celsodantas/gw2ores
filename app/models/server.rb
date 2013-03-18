0d3669dcf3a3099fa92bf6f7cb56f6113715239b
Celso Dantas <celsodantas@gmail.com>
2013-03-17 21:14:51 -0500

    refactoring the Server model

--- app/models/server.rb
+++ app/models/server.rb
@@ -1,6 +1,19 @@
 class Server < ActiveRecord::Base
   attr_accessible :name, :location
 
-  has_many :ore_nodes
   has_and_belongs_to_many :maps
+
+  has_many :ore_nodes do
+	def <<(ore)
+		# As Rails don't update counter when adding 
+		# elements directly. This code is necessary
+		server = proxy_association.owner
+
+		ore.server_id = server.id
+		Server.update_counters server.id, :ore_nodes_count => 1
+		
+		super.insert ore
+	end  	
+  end
+
 end
