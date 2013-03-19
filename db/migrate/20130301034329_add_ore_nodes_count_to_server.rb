54a6f1a79aff0c7c069129ff4a1a15e947ffa2bf
Celso Dantas <celsodantas@gmail.com>
2013-03-18 20:39:15 -0500

    removing model update from migration

--- db/migrate/20130301034329_add_ore_nodes_count_to_server.rb
+++ db/migrate/20130301034329_add_ore_nodes_count_to_server.rb
@@ -1,9 +1,5 @@
 class AddOreNodesCountToServer < ActiveRecord::Migration
   def change
     add_column :servers, :ore_nodes_count, :integer, null:false, default: 0
-
-	    Server.all.each do |server|
-	    	Server.update_counters server.id, :ore_nodes_count => server.ore_nodes.count
-	    end
   end
 end
